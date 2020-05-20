31517bd92516878168d7908366e244272f86fb83
Daniel Tahin <tahin.daniel@gmail.com>
2020-05-20 20:29:43 +0200

    Setup rotating logging

--- app.js
+++ app.js
@@ -3,13 +3,17 @@ require('full-icu')
 
 const express = require('express')
 const cors = require('cors')
-const fs = require('fs')
+var rfs = require('rotating-file-stream')
 const morgan = require('morgan')
 const path = require('path')
 const app = express()
 const port = 3001
 
-const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' })
+const accessLogStream = rfs.createStream('access.log', {
+	size: '100K',
+	maxFiles: 2,
+	path: path.join(__dirname, 'log')
+})
 
 app.use(cors())
 app.use(morgan('common', { stream: accessLogStream }))
@@ -19,16 +23,13 @@ app.get('/', (req, res) => res.redirect('/currentTime'))
 app.get('/currentTime', function (req, res) {
 	/* 
 	Request:
-	locale: string, "hu-HU" | "en-EN" | ...
-	type: string, "date" | "time", defaults to full date if omitted
-	is12: string, "true", defaults to false if omitted or not "true"
-	showSeconds: string, "true", defaults to false if omitted or not "true"
+		locale: string, "hu-HU" | "en-EN" | ...
+		type: string, "date" | "time", defaults to full date if omitted
+		is12: string, "true", defaults to false if omitted or not "true"
+		showSeconds: string, "true", defaults to false if omitted or not "true"
 	Response:
-	{"date": "2020. 05. 19.", "time": "15:44:46"} or just one of them based on the type in request
+		{"date": "2020. 05. 19.", "time": "15:44:46"} or just one of them based on the type in request
 	*/
-	console.log("-------------------------REQUEST-----------------------")
-	console.log(req.query)
-	
 	try {
 		const {locale, type, is12, showSeconds} = req.query
 		const commonOptions = {
@@ -40,16 +41,12 @@ app.get('/currentTime', function (req, res) {
 			minute: '2-digit',
 			... (showSeconds === 'true') && {second: '2-digit'}
 		}
-		//console.log(timeOptions)
 	
 		const date = new Date()
 		const dateResponse = {
 			date: date.toLocaleDateString(locale, commonOptions),
 			time: date.toLocaleTimeString(locale, {...commonOptions, ...timeOptions}),
 		}
-
-		console.log("++++++++++++++++++++++++RESPONSE++++++++++++++++++++")
-		//console.log(date)
 
 		switch(type) {
 			case 'date':
@@ -64,8 +61,6 @@ app.get('/currentTime', function (req, res) {
 	} catch (e) {
 		res.status(500).json({message: e.message})
 	}
-	
-	
 })
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
